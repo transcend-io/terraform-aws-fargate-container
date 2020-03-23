a596feb0a00cd6b3c3a8916bb0400744372889aa
dmattia <david@transcend.io>
2020-03-23 05:55:49 -0500

    Initial copy from terraform-aws-sombra

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+output json {
+  value       = module.definition.json
+  description = "JSON string representing the container definition"
+}
+
+output json_map {
+  value       = module.definition.json_map
+  description = "Object representing the container definition"
+}
+
+output secrets_policy_arn {
+  value       = aws_iam_policy.secret_access_policy.arn
+  description = "Amazon Resource Name of an IAM Policy granting access to read the SSM Parameters created in this module"
+}
+
+output container_name {
+  value       = var.name
+  description = "The name of the container"
+}
+
+output container_ports {
+  value       = var.containerPorts
+  description = <<EOF
+  Port mappings allow containers to access ports on the host container instance to send or receive traffic.
+  Each port number given will be mapped from the container to the host over the tcp protocol.
+  EOF
+}
