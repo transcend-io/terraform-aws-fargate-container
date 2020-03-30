5b3ee8beef74aadeec0043c3ce7573678d53d93e
shashiranjan84 <shashi@transcend.io>
2020-03-30 10:12:56 -0700

    Create resources only when secrets populated

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+locals {
+  all_secrets = merge(
+        var.secret_environment,
+        var.secret_environment,
+      )
+}
+
+locals {
+  has_secrets = length(local.all_secrets) > 0
+}
+
 resource "aws_ssm_parameter" "params" {
   for_each = var.secret_environment
 
@@ -27,6 +38,7 @@ resource "aws_ssm_parameter" "secret_log_options" {
 }
 
 data "aws_iam_policy_document" "secret_access_policy_doc" {
+  count = local.has_secrets ? 1 : 0
   statement {
     effect = "Allow"
     actions = [
@@ -44,9 +56,10 @@ data "aws_iam_policy_document" "secret_access_policy_doc" {
 }
 
 resource "aws_iam_policy" "secret_access_policy" {
+  count = local.has_secrets ? 1 : 0
   name_prefix = "${var.deploy_env}-${var.name}-secret-access-policy"
   description = "Gives access to read ssm env vars"
-  policy      = data.aws_iam_policy_document.secret_access_policy_doc.json
+  policy      = data.aws_iam_policy_document.secret_access_policy_doc[0].json
 }
 
 module "definition" {
